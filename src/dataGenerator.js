1742447878debda838e4fdc3f777e451c2fcc6e0
ThierryRomain <thierryr@orphic.ca>
2019-11-26 15:04:36 -0500

    implemented data generators

--- src/dataGenerator.js
+++ src/dataGenerator.js
@@ -4,6 +4,7 @@ class DataGenerator {
     this.activities = activities;
     this.timesteps = timesteps;
     this.granularity = granularity;
+    this.colors = ["rgb(255, 99, 132)","rgb(255, 159, 64)","rgb(255, 205, 86)","rgb(75, 192, 192)","rgb(54, 162, 235)","rgb(153, 102, 255)","rgb(201, 203, 207)"];
   }
 
   setTimesteps(t){
@@ -14,7 +15,7 @@ class DataGenerator {
     this.granularity = g;
   }
 
-  processDays(){
+  generateXDays(){
     let d = new Date();
     let x_axis = [];
     //rewind to start of time series
@@ -27,7 +28,7 @@ class DataGenerator {
     return x_axis;
   }
 
-  processWeeks(){
+  generateXWeeks(){
     let d = new Date();
     let x_axis = [];
     //rewind to start of the week
@@ -42,7 +43,7 @@ class DataGenerator {
     return x_axis;
   }
 
-  processMonths(){
+  generateXMonths(){
     let d = new Date();
     let x_axis = [];
     //rewind to start of the month
@@ -57,26 +58,69 @@ class DataGenerator {
     return x_axis;
   }
 
+  generateData(mult){
+    let dataArr = [];
+    for(let i in this.activities){
+      let thisObj = {};
+      thisObj.label = this.activities[i].name;
+      thisObj.borderColor = this.colors[getRandomInt(0,this.colors.length-1)];
+      thisObj.backgroundColor = thisObj.borderColor;
+      let series = [];
+      for(let j = 0; j < this.timesteps; j++){
+        let timestepTotal = 0;
+        for(let h = 0; h < mult; h++){
+          if(Math.random() < this.activities[i].prob){
+            timestepTotal += getRandomInt(this.activities[i].min,this.activities[i].max);
+          }
+        }
+        series.push(timestepTotal);
+      }
+      thisObj.data = series;
+      dataArr.push(thisObj);
+    }
+    return dataArr;
+  }
+
   generateXaxis(){
     switch(this.granularity){
       case 'days':
-      return this.processDays();
+      return this.generateXDays();
       break;
       case 'weeks':
-      return this.processWeeks();
+      return this.generateXWeeks();
       break;
       case 'months':
-      return this.processMonths();
+      return this.generateXMonths();
       break;
       default:
-      console.error('unknown granularity, will generate X axis using days. Current supported granularity are days, weeks and months.');
+      console.error('unknown granularity, will generate datasets using days. Current supported granularity are days, weeks and months.');
       return this.processDays();
       break;
     }
   }
 
   generateDataSets(){
-
+    switch(this.granularity){
+      case 'days':
+      return this.generateData(1);
+      break;
+      case 'weeks':
+      return this.generateData(7);
+      break;
+      case 'months':
+      return this.generateData(30);
+      break;
+      default:
+      console.error('unknown granularity, will generate datasets using days. Current supported granularity are days, weeks and months.');
+      return this.generateData(1);
+      break;
+    }
   }
 
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
