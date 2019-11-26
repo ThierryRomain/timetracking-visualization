02a00ea0e67076090b87218456f3db00122a4fc2
ThierryRomain <thierryr@orphic.ca>
2019-11-26 16:17:00 -0500

    adjusted driver

--- src/main.js
+++ src/main.js
@@ -1,121 +1,59 @@
-// var MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-// var COLORS = {red: 'rgb(255,0,0)', green: 'rgb(0,255,0)', blue: 'rgb(0,0,255)'};
-// var config = {
-//   type: 'line',
-//   data: {
-//     labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-//     datasets: [{
-//       label: 'My First dataset',
-//       borderColor: COLORS.red,
-//       backgroundColor: COLORS.red,
-//       data: [
-//         Math.random(),
-//         Math.random(),
-//         Math.random(),
-//         Math.random(),
-//         Math.random(),
-//         Math.random(),
-//         Math.random()
-//       ],
-//     }, {
-//       label: 'My Second dataset',
-//       borderColor: COLORS.blue,
-//       backgroundColor: COLORS.blue,
-//       data: [
-//         Math.random(),
-//         Math.random(),
-//         Math.random(),
-//         Math.random(),
-//         Math.random(),
-//         Math.random(),
-//         Math.random()
-//       ],
-//     }, {
-//       label: 'My Third dataset',
-//       borderColor: COLORS.green,
-//       backgroundColor: COLORS.green,
-//       data: [
-//         Math.random(),
-//         Math.random(),
-//         Math.random(),
-//         Math.random(),
-//         Math.random(),
-//         Math.random(),
-//         Math.random()
-//       ],
-//     }, {
-//       label: 'My Third dataset',
-//       borderColor: COLORS.red,
-//       backgroundColor: COLORS.red,
-//       data: [
-//         Math.random(),
-//         Math.random(),
-//         Math.random(),
-//         Math.random(),
-//         Math.random(),
-//         Math.random(),
-//         Math.random()
-//       ],
-//     }]
-//   },
-//   options: {
-//     responsive: true,
-//     title: {
-//       display: true,
-//       text: 'Chart.js Line Chart - Stacked Area'
-//     },
-//     tooltips: {
-//       mode: 'index',
-//     },
-//     hover: {
-//       mode: 'index'
-//     },
-//     scales: {
-//       xAxes: [{
-//         scaleLabel: {
-//           display: true,
-//           labelString: 'Month'
-//         }
-//       }],
-//       yAxes: [{
-//         stacked: false,
-//         scaleLabel: {
-//           display: true,
-//           labelString: 'Value'
-//         }
-//       }]
-//     }
-//   }
-// };
-//
-// window.onload = function() {
-//   var ctx = document.getElementById('myChart').getContext('2d');
-//   window.myLine = new Chart(ctx, config);
-// };
+//activity probabilities are always defined on the day timescale
+var activities = [
+  { name : "sleep" , prob : 1 , min : 300 , max : 600 },
+  { name : "making dreams come true" , prob : 0.8 , min : 50 , max : 300 },
+  { name : "programming" , prob : 0.7 , min : 300 , max : 800 },
+  { name : "drinking coffee" , prob : 0.5 , min : 15 , max : 60 },
+  { name : "squashing bugs" , prob : 0.4 , min : 60 , max : 330 }
+]
 
-// var colorNames = Object.keys(window.chartColors);
+const generator = new DataGenerator(activities,10,'months');
+let x_axis = generator.generateXaxis();
+let datasets = generator.generateDataSets(true);
+
+var config = {
+  data: {
+    labels: x_axis,
+    datasets: datasets
+  },
+  options: {
+    responsive: true,
+    title: {
+      display: true,
+      text: 'Timetracking data'
+    },
+    tooltips: {
+      mode: 'index',
+    },
+    hover: {
+      mode: 'index'
+    },
+    scales: {
+      xAxes: [{
+        stacked:true,
+        scaleLabel: {
+          display: true,
+        }
+      }],
+      yAxes: [{
+        stacked: true,
+        scaleLabel: {
+          display: true,
+          labelString: '% or minutes'
+        }
+      }]
+    }
+  }
+};
+
+window.onload = function() {
+  var ctx = document.getElementById('myChart').getContext('2d');
+  window.myLine = new Chart(ctx, config);
+};
 
 
-  // window.myLine.update();
+
 
 // const parser = new TimetrackingParser("./data/timetracking.csv");
 //
 // parser.load();
-
-var activities = [
-  { name : "sleep" , prob : 1 , min : 300 , max : 600 , maxfreq : 1 },
-  { name : "making dreams come true" , prob : 1 , min : 20 , max : 75 , maxfreq : null },
-  { name : "programming" , prob : 1 , min : 45 , max : 400 , maxfreq : null },
-  { name : "drinking coffee" , prob : 1 , min : 5 , max : 30 , maxfreq : 20 },
-  { name : "squashing bugs" , prob : 1 , min : 20 , max : 200 , maxfreq : null }
-]
-
-const generator = new DataGenerator(activities,100,'days');
-
-console.log(generator.generateXaxis());
-generator.setGranularity('weeks');
-console.log(generator.generateXaxis());
-generator.setGranularity('months');
-console.log(generator.generateXaxis());
-generator.setGranularity('donwjdka');
-console.log(generator.generateXaxis());
